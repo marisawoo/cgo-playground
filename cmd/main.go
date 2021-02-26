980aed8deb49e9a8403a76f0f45f0ef5057ca3b0
Marisa Woo <marisa.w@ecobee.com>
2021-02-26 18:03:45 -0500

    change print function from Go string to C string

--- cmd/main.go
+++ cmd/main.go
@@ -2,17 +2,23 @@ package main
 
 /*
 #include <stdio.h>
-void foobar() {
-	printf("hello world i am printing with C code");
+#include <stdlib.h>
+void printMsg(char *str) {
+	printf("%s", str);
 }
 */
 import "C"
+
 // comments above this line will be interpreted as C code. must be directly above, no spaced newline.
 // import "C" must also be isolated from other imports, otherwise it will fail to recognize we want to call C code
-import "fmt"
+import (
+	"fmt"
+	"unsafe"
+)
 
 func main() {
 	fmt.Printf("hello world i am printing with go\n")
-	//C.printf("hello world i am printing with cgo")
-	C.foobar()
+	cstr := C.CString("hello world i am printing with C")
+	C.foobar(cstr)
+	C.free(unsafe.Pointer(cstr)) // C string allocated in C memory, i.e. we need to free
 }
